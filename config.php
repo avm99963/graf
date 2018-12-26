f27ce62ae3983e164f433bb3efb01e166211efa9
Javier López-Contreras <38789151+javierlc2000@users.noreply.github.com>
2018-12-26 12:29:29 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Rename this file as config.php and fill in the details below.
-$conf = array();
-$conf["apiurl"] = "https://grafo.dirba.io/api.php?req=getGraph"; // L'adreça de l'API del graf original.
-$conf["password"] = ""; // La contrasenya perquè la gent pugui entrar al graf.
-
